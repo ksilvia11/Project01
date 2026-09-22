3d5e51003c19cbd807f64fe86b6d3e9462bc28d8
Silvia Kukk <ksilvia11@users.noreply.github.com>
2026-09-22 13:35:07 +0300

    Loodud paar marsuuti.

--- www_3.js
+++ www_3.js
@@ -7,7 +7,7 @@ const path = require('path');
 const fs = require ('fs').promises;
 const dateTimeET = require("./src/dateTimeET")
 const pageHead = '<!DOCTYPE html>\n<html lang="et">\n<head>\n\t<meta charset="utf-8">\n\t<title>Silvia Kukk, veevbiprogrammeerimine</title>\n</head>\n<body>\n';
-const pageBanner = '<img srcs="veebiprogrammeerimine_2026_ID.png" alt="">\n';
+const pageBanner = '<img src="veebiprogrammeerimine_2026_ID.png" alt="Banner">\n';
 const pageBody = '\t<h1>Silvia, veebiprogrammeerimine</h1>\n\t <p>See leht on loodud veebiprogrammeerimise kursusel <a href="https://www.tlu.ee">Tallinna Ülikoolis</a> ning ei sislda tõsiseltvõetavat sisu!</p>\n\t<p>Esialgu tutvusime lihtsalt HTML keelega, peatselt programmeerime.</p>\n\t<hr>';
 const pageFoot = '\n</body>\n</html>';
 
